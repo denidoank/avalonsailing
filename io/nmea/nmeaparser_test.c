1dfebaf2b714cbcb69372fac5ef7acb73ba59d9e
rekwall@google.com <rekwall@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-04-08 20:23:45 +0000

    io/nmea: First code for parsing NMEA character streams.
    Basic support for character based parsing of NMEA input. This code will be used
    by the daemons that interface with NMEA speaking sensors.
    
    R=bsuter,kendy,jpilet
    I=4312044

---
+++ io/nmea/nmeaparser_test.c
@@ -0,0 +1,65 @@
+// Copyright 2011 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+//
+// Tests the parsing functions in nmeaparser.h.
+//
+// Usage example:
+//   gcc -o nmeaparser_test nmeaparser_test.c nmeaparser.c
+//   ./nmeaparser_test < testdata/parser_test.txt > result.txt
+//   diff testdata/parser_test_expected.txt result.txt
+
+#include <stdio.h>
+#include <string.h>
+
+#include "nmeaparser.h"
+
+int main(int argc, char *argv[]) {
+  int c;
+  NmeaParser np;
+  NmeaSentence sentence;
+  npInit(&np);
+
+  FILE *f = stdin;
+  if (argc >= 2) {
+    f = fopen(argv[1], "r");
+  }
+
+  if (!f) {
+    perror(argv[1]);
+    return -1;
+  }
+
+  for(c = fgetc(f); c != EOF; c = fgetc(f)) {
+    switch (npProcessByte(&np, &sentence, c)) {
+      case NMEA_PARSER_SENTENCE_PARSED:
+        printf("Sentence parsed correctly: ");
+        break;
+
+      case NMEA_PARSER_INCORRECT_CHECKSUM:
+        printf("Incorrect checksum. Calculated checksum: 0x%X, "
+               "checksum received in sentence: 0x%X.\n",
+               sentence.checksum, sentence.receivedChecksum);
+        break;
+
+      case NMEA_PARSER_STILL_PARSING:
+        // Byte was added. Continue to next one.
+        continue;
+
+      case NMEA_PARSER_DATA_OVERFLOW_ERROR:
+        printf("Data overflow error for sentence.\n");
+        continue;
+
+      default:
+        printf("Unknown parsing state.\n");
+        continue;
+    }
+    npPrintRawSentenceData(&sentence);
+  }
+
+  printf("Summary:\n Bytes: %ld\n Errors: %ld\n Sentences: %ld\n",
+      np.totBytes,
+      np.totErr,
+      np.totSentences);
+  return 0;
+}
