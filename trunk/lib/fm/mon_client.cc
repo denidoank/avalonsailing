534b7cf73abf31ff13015bc324ad7a1974e960a8
bsuter@google.com <bsuter@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-04-20 08:10:18 +0000

    Fault management client library
    Client side library for logging and status feedback reporting to system monitor. Module should be
    initialized in each process as FM::Init(argc,argv); to recognize a set of cmdline flags:
    
    --debug: output debug log level as well to set destionations
    --logtostderr: write log messages to stderr (default disabled)
    --no-syslog: disable syslog (default syslog is enabled)
    --sysmon-socket: local dgram socket to send sysmon status reports to
    --task-name: name for this process entity
    --timeout: task entity timeout for SW watchdog keepalives
    
    R=rekwall,mariusv
    I=4330046

---
+++ trunk/lib/fm/mon_client.cc
@@ -0,0 +1,51 @@
+// Copyright 2011 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+#include "lib/fm/mon_client.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "lib/fm/log.h"
+
+#define DEFAULT_SOCKET "/dev/sysmon"
+
+MonClient::MonClient() {
+  SetDestination(DEFAULT_SOCKET);
+  sockfd_ = socket(AF_UNIX, SOCK_DGRAM, 0);
+  if (sockfd_ == -1) {
+    FM_LOG_PERROR("Opening monitoring client socket");
+  } else {
+    fcntl(sockfd_, F_SETFD, FD_CLOEXEC);
+  }
+}
+
+MonClient::~MonClient() {
+  if (sockfd_ != -1) {
+    close(sockfd_);
+  }
+}
+
+void MonClient::SetDestination(const char *address) {
+  monitor_address_.sa_family = AF_UNIX;
+  strncpy(monitor_address_.sa_data, address, sizeof(monitor_address_.sa_data));
+}
+
+void MonClient::SendMonMsg(const char *fmt, ...) const {
+   va_list arg_ptr;
+   char buffer[512];
+   int len;
+
+   va_start(arg_ptr, fmt);
+   len = vsnprintf(buffer, sizeof(buffer), fmt, arg_ptr);
+   va_end(arg_ptr);
+
+
+   if (sendto(sockfd_, buffer, len, 0,
+              &monitor_address_, sizeof(monitor_address_)) == -1) {
+     FM_LOG_PERROR("Sending report to sysmon");
+   }
+}
---
+++ trunk/lib/util/stopwatch.h
@@ -0,0 +1,31 @@
+// Copyright 2011 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+#ifndef LOG_UTIL_STOPWATCH_H__
+#define LOG_UTIL_STOPWATCH_H__
+
+#include <stddef.h>
+#include <sys/time.h>
+
+// Utility class to keep track of elapsed real-time in ms
+// since the last set.
+class StopWatch {
+ public:
+  StopWatch() { Set(); }
+  void Set () {
+    gettimeofday(&start_time_, NULL);
+  }
+  // Return elapsed time in milliseconds
+  long Elapsed() const {
+    timeval current_time;
+    gettimeofday(&current_time, NULL);
+    return static_cast<long>((current_time.tv_sec * 1000LL +
+                              current_time.tv_usec / 1000LL) -
+                             (start_time_.tv_sec * 1000LL +
+                              start_time_.tv_usec / 1000LL));
+  }
+ private:
+  timeval start_time_;
+};
+
+#endif // LOG_UTIL_STOPWATCH_H__
