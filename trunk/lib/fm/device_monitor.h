1cccf8098342ce2873d53e232d02555cdd6a3904
bsuter@google.com <bsuter@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-05-12 15:34:04 +0000

    FM client for HW devices & assert/log_fatal support
    FM client for sensor and actuator entities to report their liveness and error rates.
    Assert macro and log_fatal level to crash a task in case of error with flushing
    of telemetry to sysmon before.
    
    R=reckwall,mariusv
    I=4444074

---
+++ trunk/lib/fm/device_monitor.h
@@ -0,0 +1,38 @@
+// Copyright 2011 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+#ifndef LIB_FM_DEVICE_MONITOR_H__
+#define LIB_FM_DEVICE_MONITOR_H__
+
+#include "lib/fm/constants.h"
+#include "lib/util/stopwatch.h"
+
+// Fault management client for a hardware peripheral device
+// (sensor or actuator) which is expected to produce or
+// consume a periodic stream of I/O events.
+class DeviceMonitor {
+ public:
+
+  DeviceMonitor(const char *name, long timeout_ms);
+
+  // For each iteration report if the operations was
+  // a success or failure. Iterations which result in
+  // hanging read or read timeouts should not be reported
+  // as data error (device will eventually be timed out).
+  void DataValid();
+  void DataError();
+
+  // Explicitly report task status to system monitor (optional)
+  void SetStatus(FM_STATUS status, const char *msg);
+
+ private:
+  void Keepalive(bool force = false);
+
+  StopWatch timer_;
+  long timeout_ms_;
+  char device_name_[30];
+  long valid_count_;
+  long error_count_;
+};
+
+#endif // LIB_FM_LOG_H__
