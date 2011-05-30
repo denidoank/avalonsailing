6fb327da2a0823820303760fde7f6749cedc4e73
grundmann@google.com <grundmann@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-05-30 07:18:34 +0000

    Component: Sail controller fix and optimal gamma sail.
    Fix of a missing initialization.
    
    R=rekwall,jpilet
    I=4543072

--- common/check.h
+++ common/check.h
@@ -13,10 +13,12 @@
 
 #define CHECK(a) assert(a)
 #define CHECK_EQ(a, b) assert((a) == (b))
-#define CHECK_FLOAT_EQ(a, b) assert(fabs((a) - (b)) < 1E-6)
+#define CHECK_FLOAT_EQ(a, b) assert(fabs((a) - (b)) < 1E-5)
 #define CHECK_LT(a, b) assert((a) < (b))
 #define CHECK_LE(a, b) assert((a) <= (b))
 #define CHECK_GE(a, b) assert((a) >= (b))
 #define CHECK_GT(a, b) assert((a) > (b))
+#define CHECK_INTERVAL(a, x, b) assert((a) < (x) && (x) < (b))
+
 
 #endif  // COMMON_CHECK_H
--- helmsman/optimal_gamma_sail_test.cc
+++ helmsman/optimal_gamma_sail_test.cc
@@ -5,93 +5,81 @@
 
 #include "helmsman/optimal_gamma_sail.h"
 
-#include "common/testing.h"
 #include "common/convert.h"
-
-// Returns optimal gamma sail for the given
-// direction of the apparent wind vector relative to the boats x-axis
-// (in degrees, 90 degrees is wind from port side, 0 degree is wind
-// from behind) in [0, 360) and
-// the wind speed in m/s. wind_speed >= 0 
-double OptimalGammaSail(double alpha_apparent_wind, double wind_speed);
-
-// As OptimalGammaSail. Additionally the forward force F_x in Newton and
-// the heel angle, rotation around the x-axis, indegrees is produced. 
-void OptimalTriplett(double alpha_apparent_wind,
-                     double magnitude_wind,
-                     double* gamma_sail,
-                     double* force,
-                     double* heel);
+#include "lib/testing/testing.h"
 
 TEST(OptimalGammaSail, All) {
+  double wind_speed = 5;
+  double gamma_sail = 0;
+  double force = 0;
+  double heel = 0;
+  double gamma_sail_2 = 0;
 
-double wind_speed = 5;
-double gamma_sail = 0;
-double force = 0;
-double heel = 0;
+  printf("     apparent/degree gamma_sail/degree, force/N,       heel/degree\n");
+  for (double apparent = -20; apparent < 400; apparent += 0.742) {
+    OptimalTriplett(Deg2Rad(apparent),
+                    wind_speed,
+                    &gamma_sail,
+                    &force,
+                    &heel);
+    gamma_sail_2 = OptimalGammaSail(Deg2Rad(apparent), wind_speed);
+    EXPECT_FLOAT_EQ(gamma_sail, gamma_sail_2);              
+    printf("%15g %15g %15g %15g\n",
+           apparent, Rad2Deg(gamma_sail), force, Rad2Deg(heel));                
+    EXPECT_GT(force, -100);
+  }
 
+  printf("     apparent/degree gamma_sail/degree, force/N,       heel/degree\n");
+  for (double apparent = 0; apparent < 140; apparent += 0.5) {
+    OptimalTriplett(Deg2Rad(apparent),
+                    wind_speed,
+                    &gamma_sail,
+                    &force,
+                    &heel);
+    gamma_sail_2 = OptimalGammaSail(Deg2Rad(apparent), wind_speed);
+    EXPECT_FLOAT_EQ(gamma_sail, gamma_sail_2);              
+    printf("%15g %15g %15g %15g\n",
+           apparent, Rad2Deg(gamma_sail), force, Rad2Deg(heel));                
+    EXPECT_LT(gamma_sail, Deg2Rad(-15));
+    EXPECT_GT(gamma_sail, Deg2Rad(-120));
+    EXPECT_GT(force, 0);
+    EXPECT_GT(heel, Deg2Rad(-2));
+  }
 
+  printf("     apparent/degree gamma_sail/degree, force/N,       heel/degree\n");
+  for (double apparent = -0.5; apparent > -140; apparent -= 0.5) {
+    OptimalTriplett(Deg2Rad(apparent),
+                    wind_speed,
+                    &gamma_sail,
+                    &force,
+                    &heel);
+    gamma_sail_2 = OptimalGammaSail(Deg2Rad(apparent), wind_speed);
+    EXPECT_FLOAT_EQ(gamma_sail, gamma_sail_2);              
+    printf("%15g %15g %15g %15g\n",
+           apparent, Rad2Deg(gamma_sail), force, Rad2Deg(heel));                
+    EXPECT_GT(gamma_sail, Deg2Rad(15));
+    EXPECT_LT(gamma_sail, Deg2Rad(120));
+    EXPECT_GT(force, 0);
+    EXPECT_LT(heel, Deg2Rad(2));
+  }
 
-printf("apparent/degree gamma_sail/degree, force/N, heel/degree\n");
-for (double apparent = -20; apparent < 400; apparent += 0.742) {
-  OptimalTriplett(Deg2Rad(apparent),
+  double apparent = 0;
+  wind_speed = 5;
+  OptimalTriplett(apparent,
                   wind_speed,
                   &gamma_sail,
                   &force,
                   &heel);
-  printf("%15g %15g %15g %15g\n",
-         apparent, Rad2Deg(gamma_sail), force, Rad2Deg(heel));                
-  EXPECT_GT(force, -100);
-}
-
-printf("apparent/degree gamma_sail/degree, force/N, heel/degree\n");
-for (double apparent = 1; apparent < 180; apparent += 0.5) {
-  OptimalTriplett(Deg2Rad(apparent),
+  // double wind speed, four times the force
+  wind_speed *= 2;
+  double force2;
+  OptimalTriplett(apparent,
                   wind_speed,
                   &gamma_sail,
-                  &force,
+                  &force2,
                   &heel);
-  printf("%15g %15g %15g %15g\n",
-         apparent, Rad2Deg(gamma_sail), force, Rad2Deg(heel));                
-  EXPECT_LT(gamma_sail, -15);
-  EXPECT_GT(gamma_sail, -120);
-  EXPECT_GT(force, 0);
-  EXPECT_GT(heel, -2);
-}
-
-printf("apparent/degree gamma_sail/degree, force/N, heel/degree\n");
-for (double apparent = -1; apparent > -180; apparent -= 0.5) {
-  OptimalTriplett(Deg2Rad(apparent),
-                  wind_speed,
-                  &gamma_sail,
-                  &force,
-                  &heel);
-  printf("%15g %15g %15g %15g\n",
-         apparent, Rad2Deg(gamma_sail), force, Rad2Deg(heel));                
-  EXPECT_GT(gamma_sail, 15);
-  EXPECT_LT(gamma_sail, 120);
-  EXPECT_GT(force, 0);
-  EXPECT_LT(heel, 2);
-}
-
-double apparent = 0;
-wind_speed = 5;
-OptimalTriplett(apparent,
-                wind_speed,
-                &gamma_sail,
-                &force,
-                &heel);
-// double wind speed, four times the force
-wind_speed *= 2;
-double force2;
-OptimalTriplett(apparent,
-                wind_speed,
-                &gamma_sail,
-                &force2,
-                &heel);
-EXPECT_LT(3.9 * force, force2);
-EXPECT_GT(4.1 * force, force2);
-
+  EXPECT_LT(3.6 * force, force2);
+  EXPECT_GT(4.4 * force, force2);
 }
 
 int main(int argc, char* argv[]) {
