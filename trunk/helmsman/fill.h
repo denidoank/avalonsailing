ae127d5b9256972ff48adfa10f939f5eb21e690a
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-06-08 13:18:38 +0000

    helmsman: glue to rudderd
    R=grundmann
    I=4602041

--- trunk/helmsman/fill.h
+++ trunk/helmsman/fill.h
@@ -1,40 +1,36 @@
 // Copyright 2011 The Avalon Project Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0
 // that can be found in the LICENSE file.
-// Steffen Grundmann, May 2011
+// Steffen Grundmann, June 2011
 
-// Sail brake status is managed by the drive interface. 
-// All angle values in degrees. For the drives the [-180, 180] convention
-// is used.
-// Constructors, Reset, Check and conversion functions are provided for.
+// Fill internal data structures from raw input strings
 
-#ifndef HELMSMAN_DRIVE_DATA_H
-#define HELMSMAN_DRIVE_DATA_H
+#ifndef HELMSMAN_FILL_H
+#define HELMSMAN_FILL_H
 
 #include <string>
 
-struct DriveReferenceValuesRad;
+#include "helmsman/imu.h"
+#include "helmsman/wind.h"
+#include "helmsman/drive_data.h"
 
-struct DriveActualValues {
-  DriveActualValues();
-  //DriveActualValues(const DriveActualValuesRad&);
-  void Reset();
-  void Check();
-  double gamma_rudder_left_deg;
-  double gamma_rudder_right_deg;
-  double gamma_sail_deg;
-  // homed == "ready to be used". If false, the commands will be ignored.
-  bool homed_rudder_left;
-  bool homed_rudder_right;
-  bool homed_sail;
-};
+using namespace std;
 
+bool FillImu(const string& raw, 
+             Imu* imu);
+
+bool FillWind(const string& raw, 
+              WindRad* wind_rad);
+
+bool FillDrivesActual(const string& raw, 
+                      DriveActualValuesRad* drive_actual_rad);
+
+/* for refernce
 // All values in degrees. For the drives the [-180, 180] convention is used.
 // The drive interface handles the wrap around from -179 to 179 degrees.
 struct DriveReferenceValues {
   DriveReferenceValues();
   DriveReferenceValues(const DriveReferenceValuesRad& ref_rad);
-  std::string ToString() const;
   void Reset();
   void Check();
   double gamma_rudder_star_left_deg;
@@ -45,7 +41,6 @@ struct DriveReferenceValues {
 // Internally used radians versions.
 struct DriveActualValuesRad {
   DriveActualValuesRad();
-  DriveActualValuesRad(const std::string& kvline);
   DriveActualValuesRad(const DriveActualValues& act_deg);
   void Reset();
   double gamma_rudder_left_rad;
@@ -68,4 +63,6 @@ struct DriveReferenceValuesRad {
   double gamma_sail_star_rad;
 };
 
-#endif  // HELMSMAN_DRIVE_DATA_H
+*/
+
+#endif  // HELMSMAN_FILL_H
---
+++ trunk/helmsman/helmsman_main.cc
@@ -0,0 +1,181 @@
+// Copyright 2011 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+//
+// Helmsman daemon for conrolling boat heading and sail
+// ./helmsman --logtostderr --no-syslog --task-name=helmsman --timeout=10 --debug \
+//        --imu=testdata/imu.nme --wind=testdata/wind.nme \
+//        --drives=testdata/drive_in.nme \
+//        --command=testdata/commands.txt --skipper_file=testdata/skipper.txt
+// ./helmsman --logtostderr --no-syslog --task-name=helmsman --timeout=10 \
+//       --debug  --imu=testdata/imu.nme --wind=testdata/wind.nme  --drives=testdata/drive_in.nme  --command=testdata/commands.txt --skipper=testdata/skipper.txt
+
+
+#include "io/ipc/producer_consumer.h"
+#include "lib/fm/fm.h"
+#include "lib/fm/log.h"
+//#include "modem/modem.h"
+
+#include "helmsman/drive_data.h"
+#include "helmsman/fill.h"
+#include "helmsman/imu.h"
+
+#include "helmsman/sampling_period.h"
+#include "helmsman/ship_control.h"
+#include "helmsman/wind.h"
+#include "lib/util/stopwatch.h"
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+
+using namespace std;
+
+const size_t kFlagSize = 256;
+
+static char imu_file[kFlagSize] = "";
+static char wind_file[kFlagSize] = "";
+static char drives_file[kFlagSize] = "";
+static char skipper_file[kFlagSize] = "";
+static char command_file[kFlagSize] = "";
+
+
+static void Init(int argc, char** argv) {
+  static const struct option long_options[] = {
+    { "imu", 1, NULL, 0},
+    { "wind", 1, NULL, 0},
+    { "drives", 1, NULL, 0},
+    { "command", 1, NULL, 0},
+    { "skipper", 1, NULL, 0},
+    { NULL, 0, NULL, 0 }
+  };
+  
+  optind = 1;
+  opterr = 0;
+  int opt_index;
+  int opt;
+  while ((opt = getopt_long(argc, argv, "", long_options, &opt_index)) != -1) {
+    if (opt != 0)  // Invalid argument, probably used by other module.
+      continue;
+    switch(opt_index) {
+      case 0:  // --imu=...
+        strncpy(imu_file, optarg, kFlagSize);
+        break;
+      case 1:  // --wind=...
+        strncpy(wind_file, optarg, kFlagSize);
+        break;
+      case 2:  // --drives=...
+        strncpy(drives_file, optarg, kFlagSize);
+        break;
+      case 3:  // --commands=...
+        strncpy(command_file, optarg, kFlagSize);
+        break;
+      case 4:  // --skipper=...
+        strncpy(skipper_file, optarg, kFlagSize);
+        break;
+      default:
+        FM_LOG_INFO("usage: %s -i imu_dev] [-w wind_dev]\n"
+                    "\t -d drives\n"
+                    "\t -c command_dev -s skipper_dev\n",
+                    argv[0]);
+        exit(0);
+    }
+  }
+}
+
+long long NowMicros() {
+  return StopWatch::GetTimestampMicros();
+}
+
+long long WaitUntil(long long next, long long period) {
+  long long now = NowMicros();
+  if (next > now) {
+    sleep((next - now) / 1E6);
+  } else {
+    FM_LOG_INFO("Helmsman: to late %lld micros", now - next);
+  }
+  return next + period;
+}
+
+
+int main(int argc, char** argv) {
+  FM::Init(argc, argv);
+  Init(argc, argv);
+
+  FM_LOG_INFO("Helmsman: Using IO files: %s and wind sensor: %s "
+              "drives_file %s command_file %s skipper_file%s\n",
+              imu_file, wind_file, drives_file, command_file, skipper_file);
+  if (strlen(imu_file) == 0) {
+    FM_LOG_FATAL("No imu_file specified.");
+  }
+  if (strlen(wind_file) == 0) {
+    FM_LOG_FATAL("No wind_file specified.");
+  }
+  if (strlen(drives_file) == 0) {
+    FM_LOG_FATAL("No drives_file specified.");
+  }
+  if (strlen(command_file) == 0) {
+    FM_LOG_FATAL("No command_file specified.");
+  }
+  if (strlen(skipper_file) == 0) {
+    FM_LOG_FATAL("No skipper_file specified.");
+  }
+
+  ShipControl m;
+  Consumer imu_consumer(imu_file);
+  Consumer wind_consumer(wind_file);
+  Consumer command_consumer(command_file);
+  Consumer skipper_consumer(skipper_file);
+  ProducerConsumer drive(drives_file);
+  //Producer skipper_producer(true_wind_file);
+
+  string imu_raw, wind_raw, drives_raw, command_raw, skipper_raw;
+  Imu imu;
+  WindRad wind;
+
+  if (imu_consumer.Consume(&imu_raw)) {
+    FM_LOG_INFO("Found imu data: %s \n", imu_raw.c_str());
+    FillImu(imu_raw, &imu);
+  }
+  if (wind_consumer.Consume(&wind_raw)) {
+    FM_LOG_INFO("Found wind data: %s \n", wind_raw.c_str());
+    FillWind(wind_raw, &wind);
+  }
+  if (command_consumer.Consume(&command_raw)) {
+    FM_LOG_INFO("Found command data: %s \n", command_raw.c_str());
+  }
+  if (skipper_consumer.Consume(&skipper_raw)) {
+    FM_LOG_INFO("Found skipper data: %s \n", skipper_raw.c_str());
+  }
+
+  DriveReferenceValuesRad drive_reference;
+
+  int rounds = 3;
+  long long next_micros = NowMicros() + kSamplingPeriod * 1E6;
+  // wait until the next tick passed.
+  next_micros = WaitUntil(next_micros, kSamplingPeriod * 1E6);
+  while (true) {
+    if (imu_consumer.Consume(&imu_raw)) {
+      FM_LOG_INFO("Found imu data: %s \n", imu_raw.c_str());
+      FillImu(imu_raw, &imu);
+    }
+
+    string line;
+    drive.Consume(&line);
+    DriveActualValuesRad drive_actual(line);
+
+    drive_reference.Reset();     /// fill this somehow
+
+    {
+      DriveReferenceValues out(drive_reference);
+      drive.Produce(out.ToString());
+    }
+
+    FM::Keepalive();
+
+    next_micros = WaitUntil(next_micros, kSamplingPeriod * 1E6);
+    if (--rounds <= 0)
+      break;
+  }
+}
