2747ba016638f49ce1aa7a0f10860b82a2b47e7b
bsuter@google.com <bsuter@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-06-22 00:24:49 +0000

    Get config property values with type conversion
    - Array of hard-coded default properties with abilities to override values from a file
    - Conversion from string to long, double & boolean
    - If anything goes wrong (parse error, property key does not exist) return hard-coded default value at point of call.
    
    R=rekwall
    I=4629052

--- trunk/lib/config/properties.h
+++ trunk/lib/config/properties.h
@@ -4,11 +4,33 @@
 #ifndef LIB_CONFIG_PROPERTIES_H__
 #define LIB_CONFIG_PROPERTIES_H__
 
+#include <string>
+
 #include "io/ipc/key_value_pairs.h"
 
-// Load key/value properties by merging an arary of hard-coded
-// defaults with additional entries loaded from a file.
-bool LoadProperties(const char *defaults[][2], const char *filename,
-                    KeyValuePair *properties);
+class Properties {
+ public:
+  Properties() { }
+  // Initialize with an array of hard-coded values
+  Properties(const char *defaults[][2]);
+
+  // Merge the content of a key-value file with the current property map
+  // overriding existing values.
+  bool LoadFromFile(const char *filename);
+
+  // Return a converted value of the corresponding type from the properties
+  // map, otherwise return the provided hard-coded default value.
+  std::string Get(const std::string &key,
+                  const std::string &default_string) const;
+  double Get(const std::string &key, double default_double) const;
+  long Get(const std::string &key, long default_long) const;
+  bool Get(const std::string &key, bool default_bool) const;
+
+  // Get const references to underlying key-value pairs object.
+  const KeyValuePair &GetKeyVals() { return properties_; }
+
+ private:
+  KeyValuePair properties_;
+};
 
 #endif // LIB_CONFIG_PROPERTIES_H__
