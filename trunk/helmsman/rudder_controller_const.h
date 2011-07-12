6b1aaf1ed90838e8040471a849fc4d3105eb5dd0
grundmann@google.com <grundmann@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-07-12 06:33:26 +0000

    component: Short 1-line description of what changed
    Long explanation of *why* things were changed, etc.
    
    R=lvd,rekwall
    I=4695044

--- trunk/helmsman/rudder_controller_const.h
+++ trunk/helmsman/rudder_controller_const.h
@@ -10,6 +10,26 @@
 const double kStateFeedback1 = 452.39;
 const double kStateFeedback2 = 563.75;
 const double kStateFeedback3 = 291.71;
- 
-#endif  // HELMSMAN_RUDDER_CONTROLLER_CONST_H
 
+
+// Closed loop 10s response time. 
+// const double kStateFeedback1 = 226.195;
+// const double kStateFeedback2 = 140.938;
+// const double kStateFeedback3 = 36.463;
+
+// Closed loop 10s response time, energy-saving (one could say mini-phlegmatic) flavour. 
+// const double kStateFeedback1 = 226.195;
+// const double kStateFeedback2 = 114.290;
+// const double kStateFeedback3 = 19.720;
+
+// Closed loop 25s response time, very energy-saving (one could say phlegmatic) flavour. 
+// const double kStateFeedback1 = 179.0708;
+// const double kStateFeedback2 = 72.8377;
+// const double kStateFeedback3 = 9.8600;
+
+// Closed loop 45s response time, absolutely energy-saving (one could say mega-phlegmatic) flavour. 
+// const double kStateFeedback1 = 89.5354;
+// const double kStateFeedback2 = 18.2094;
+// const double kStateFeedback3 = 1.2325;
+
+#endif  // HELMSMAN_RUDDER_CONTROLLER_CONST_H
