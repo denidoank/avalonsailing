07d4f01e6008e22e181528d0298f40abef26ffc4
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-07-19 13:10:57 +0000

    io and skipper: in-field fixes from last thursday.

--- io/rudderd/rudderd_main.c
+++ io/rudderd/rudderd_main.c
@@ -31,11 +31,11 @@
 #include "rudder.h"
 
 struct MotorParams motor_params[] = {
-        { "LEFT",  0x09011145,  90.0, -90.0, 0, -288000 },
+        { "LEFT",  0x09011145, 100.0, -80.0, 0, -288000 },
         { "RIGHT", 0x09010537, -90.0,  90.0, 0,  288000 },
 // sail and bmmh *must* be 360 degree ranges
-        { "SAIL",  0x09010506,  -180, 180.0, -615000, 615000 },
-	{ "BMMH",  0x00001227,  -180, 180.0, -2048, 2048 }, // 4096 tics for a complete rotation
+        { "SAIL",  0x09010506,  -180, 180.0, 615000, -615000 },
+	{ "BMMH",  0x00001227,  -180, 180.0, 2048, -2048 }, // 4096 tics for a complete rotation
 };
 
 static const char* version = "$Id: $";
