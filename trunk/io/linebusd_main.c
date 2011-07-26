66fee55c89a4a006568fea1a56f3f1989387613f
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-07-26 07:44:23 +0000

    io: new linebusd and more features to plug

---
+++ trunk/io/linebusd_main.c
@@ -0,0 +1,340 @@
+// Copyright 2011 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+//
+// Daemon to read lines from stdin and publish them to any
+// connected client.
+//
+//
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/wait.h>
+#include <syslog.h>
+#include <time.h>
+#include <unistd.h>
+
+// -----------------------------------------------------------------------------
+//   Together with getopt in main, this is our minimalistic UI
+// -----------------------------------------------------------------------------
+// static const char* version = "$Id: $";
+static const char* argv0;
+static int verbose = 0;
+static int debug = 0;
+
+static void
+crash(const char* fmt, ...)
+{
+	va_list ap;
+	char buf[1000];
+	va_start(ap, fmt);
+	vsnprintf(buf, sizeof(buf), fmt, ap);
+	if (debug)
+		fprintf(stderr, "%s:%s%s%s\n", argv0, buf,
+			(errno) ? ": " : "",
+			(errno) ? strerror(errno):"" );
+	else
+		syslog(LOG_CRIT, "%s%s%s\n", buf,
+		       (errno) ? ": " : "",
+		       (errno) ? strerror(errno):"" );
+	exit(1);
+	va_end(ap);
+	return;
+}
+
+static void
+usage(void)
+{
+	fprintf(stderr,
+		"usage: %s [options] /path/to/socket\n"
+		"options:\n"
+		"\t-d debug (don't go daemon, don't syslog)\n"
+		, argv0, argv0);
+	exit(2);
+}
+
+//
+struct LineBuffer {
+	char line[1024];
+	int head;
+	int discard;
+	int tail;
+};
+
+// Reads from fd into the linebuffer, returns 0 if there is a \n terminated
+// line, EAGAIN if there is an incomplete line, or errno from read(2)
+// otherwise.  if the buffer overflows before EOL sets the discard flag which will
+// cause the current line to be discarded until an EOL is found.
+// the line will be 0-terminated.
+static int lb_read(int fd, struct LineBuffer* lb) {
+	int n = read(fd, lb->line+lb->head, sizeof(lb->line) - lb->head - 1);
+	switch (n) {
+	case 0:
+		return EOF;
+	case -1:
+		return errno;
+	}
+
+	lb->line[lb->head+n] = 0;
+	char* eol = strchr(lb->line+lb->head, '\n');
+	if (eol) {
+		lb->head = 0;
+		if (!lb->discard) return 0;  // lb->line starts with a complete line
+		lb->discard = 0;
+	}
+	
+	// no EOL || discard
+	lb->head += n;
+	if (lb->head == sizeof(lb->line) - 1) lb->discard = 1;
+	if (lb->discard) lb->head = 0;
+	return EAGAIN;
+}
+
+static void lb_putline(struct LineBuffer* lb, const char* line) {
+	strncpy(lb->line, line, sizeof lb->line - 1);
+	lb->line[sizeof lb->line - 1] = 0;
+	lb->head = strlen(lb->line);
+}
+
+static int lb_pending(struct LineBuffer* lb) {
+	return lb->head > 0;
+}
+
+static int lb_write(int fd, struct LineBuffer* lb) {
+	int n = write(fd, lb->line + lb->tail, lb->head - lb->tail);
+	if (n < 0) return errno;
+	lb->tail += n;
+	if (lb->tail != lb->head) return EAGAIN;
+	lb->head = 0;
+	lb->tail = 0;
+	return 0;
+}
+
+
+// -----------------------------------------------------------------------------
+//   Linked list of open client sockets.
+//     new_client    accepts a new connection from a listening socket
+//     client_puts   writes a single line to the clients buffer
+//     client_flush  tries to write out the output buffer, and detects eof
+//     reap_clients  frees all closed clients from the list.
+//
+//   See the main loop below on sample usage.
+// -----------------------------------------------------------------------------
+static struct Client {
+	struct Client* next;
+	int fd;	// set to -1 when puts or flush detects EOF
+	struct LineBuffer in;
+	struct LineBuffer out;
+	struct sockaddr_un addr;
+	socklen_t addrlen;
+} *clients = NULL;
+
+static struct Client*
+new_client(int sck)
+{
+	struct Client* cl = (struct Client*)malloc(sizeof *cl);
+	memset(cl, 0, sizeof(*cl));
+	cl->addrlen = sizeof(cl->addr);
+	cl->fd = accept(sck, (struct sockaddr*)&cl->addr, &cl->addrlen);
+	if (cl->fd < 0) {
+		// probably hung up before we got here, no reason to crash.
+		if (debug) fprintf(stderr, "accept:%s\n", strerror(errno));
+		free(cl);
+		return NULL;
+	}
+	if (debug) fprintf(stderr, "New client: %d\n", cl->fd);
+        if (fcntl(cl->fd,  F_SETFL, O_NONBLOCK) < 0) crash("fcntl(in)");
+	cl->next = clients;
+	clients = cl;
+	return cl;
+}
+
+static void
+set_fd(fd_set* s, int* maxfd, int fd)
+{
+	FD_SET(fd, s);
+	if (*maxfd < fd) *maxfd = fd;
+}
+
+static void
+client_setfds(struct Client* client, fd_set* rfds, fd_set* wfds, int* max_fd)
+{
+	if (client->fd < 0) return;
+	if (lb_pending(&client->out)) set_fd(wfds, max_fd, client->fd);
+	set_fd(rfds, max_fd, client->fd);
+}
+
+static void
+client_puts(struct Client* client, const char* line)
+{
+	if (client->fd < 0) return;
+	lb_putline(&client->out, line);
+}
+
+static void
+client_flush(struct Client* client)
+{
+	if (client->fd < 0) return;
+	if (!lb_pending(&client->out)) return;
+	int r = lb_write(client->fd, &client->out);
+	if ((r != 0) && (r != EAGAIN)) {
+		close(client->fd);
+		client->fd = -1;
+	}
+}
+
+static const char*
+client_gets(struct Client* client) {
+	if (client->fd < 0) return;
+	int r = lb_read(client->fd, &client->in);
+	if (r == 0) return client->in.line;
+	if (r != EAGAIN) {
+		close(client->fd);
+		client->fd = -1;
+	}
+	return NULL;
+}
+
+static void
+reap_clients()
+{
+	struct Client** prevp = &clients;
+	while (*prevp) {
+		struct Client* curr = *prevp;
+		if(curr->fd < 0) {
+			if (debug) fprintf(stderr, "Closed client.\n");
+			*prevp = curr->next;
+			free(curr);
+		} else {
+			prevp = &curr->next;
+		}
+	}
+}
+
+// -----------------------------------------------------------------------------
+//   Main.
+//   Parse options, open socket, optionally go daemon
+//   and enter main loop.
+// -----------------------------------------------------------------------------
+int main(int argc, char* argv[]) {
+
+	int ch;
+	char* path_to_socket = NULL;
+
+	argv0 = strrchr(argv[0], '/');
+	if (argv0) ++argv0; else argv0 = argv[0];
+
+	while ((ch = getopt(argc, argv, "dhv")) != -1){
+		switch (ch) {
+		case 'd': ++debug; break;
+		case 'v': ++verbose; break;
+		case 'h':
+		default:
+			usage();
+		}
+	}
+
+	argv += optind;
+	argc -= optind;
+
+	if (argc != 1) usage();
+
+	argv0 = strrchr(argv[0], '/');
+	if (argv0) ++argv0; else argv0 = argv[0];
+
+	if (!debug) openlog(argv0, LOG_PERROR, LOG_DAEMON);
+
+	// Set up socket.
+	unlink(argv[0]);
+	int sck = socket(AF_LOCAL, SOCK_STREAM, 0);
+	if (sck < 0) crash("socket");
+	struct sockaddr_un srvaddr = { AF_LOCAL, 0 };
+	strncpy(srvaddr.sun_path, argv[0], sizeof srvaddr.sun_path );
+	if (bind(sck, (struct sockaddr*) &srvaddr, sizeof srvaddr) < 0) crash("bind(%S)", argv[0]);
+	if (listen(sck, 8) < 0) crash("listen");
+
+	if (debug) fprintf(stderr, "created socket:%s\n", argv[0]);
+
+	// Go daemon and write pidfile.
+	if (!debug) {
+		daemon(0,1);
+
+		char* path_to_pidfile = NULL;
+		asprintf(&path_to_pidfile, "%s.pid", argv[0]);
+		FILE* pidfile = fopen(path_to_pidfile, "w");
+		if(!pidfile) crash("writing pidfile");
+		fprintf(pidfile, "%d\n", getpid());
+		fclose(pidfile);
+		free(path_to_pidfile);
+
+		syslog(LOG_INFO, "Started.");
+	}
+
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) crash("signal");
+
+	// Main Loop
+	for(;;) {
+
+		fd_set rfds;
+		fd_set wfds;
+		int max_fd = -1;
+		FD_ZERO(&rfds);
+		FD_ZERO(&wfds);
+
+		set_fd(&rfds, &max_fd, sck);
+
+		struct Client* cl;
+		for (cl = clients; cl; cl = cl->next)
+			client_setfds(cl, &rfds, &wfds, &max_fd);
+
+		sigset_t empty_mask;
+		sigemptyset(&empty_mask);
+		int r = pselect(max_fd + 1, &rfds, &wfds, NULL, NULL, &empty_mask);
+		if (r == -1 && errno != EINTR) crash("pselect");
+
+		if (debug) fprintf(stderr, "woke up %d\n", r);
+
+		// Handle clients.
+		if (FD_ISSET(sck, &rfds)) new_client(sck);
+
+		for (cl = clients; cl; cl = cl->next)
+			if (FD_ISSET(cl->fd, &wfds))
+				client_flush(cl);
+
+		struct Client *cl2;
+		for (cl = clients; cl; cl = cl->next) {
+			if (cl->fd < 0) continue;
+			if (!FD_ISSET(cl->fd, &rfds)) continue;
+
+			const char* line = client_gets(cl);
+			if (!line) continue;
+			if (debug > 1) puts(line);
+			for (cl2 = clients; cl2; cl2 = cl2->next) {
+				if (cl == cl2) continue;
+				if (cl2->fd < 0) continue;
+				if (lb_pending(&cl2->out)) {
+					if (debug) fprintf(stderr, "Dropping output to client.\n");
+					continue;
+				}
+				client_puts(cl2, line);
+			}
+		}
+
+		reap_clients();
+
+	} // main loop
+
+	crash("Terminating");
+
+	return 0;
+}
--- trunk/io/plug_main.c
+++ trunk/io/plug_main.c
@@ -2,11 +2,12 @@
 // Use of this source code is governed by the Apache License 2.0
 // that can be found in the LICENSE file.
 //
-// Netcat like 'plug' for unix sockets. Useful for testing eposd.
+// Netcat like 'plug' for unix sockets.
 //
 //
 
 #include <errno.h>
+#include <getopt.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +18,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-const char* argv0;
+static const char* argv0;
+static int debug;
+static int verbose;
 
 static void
 crash(const char* fmt, ...)
@@ -34,47 +37,106 @@ crash(const char* fmt, ...)
         return;
 }
 
+
+static void
+usage(void)
+{
+	fprintf(stderr,
+		"usage: %s [options] /path/to/socket\n"
+		"options:\n"
+		"\t-i input only\n"
+		"\t-o output only\n"
+		"\t-d debug (don't go daemon, don't syslog)\n"
+		, argv0, argv0);
+	exit(2);
+}
+
 int main(int argc, char* argv[]) {
+	int ch;
+	int noin = 0;
+	int noout = 0;
+
+	pid_t s_to_out_pid = -1;
+	pid_t in_to_s_pid  = -1;
+
         argv0 = argv[0];
-        if (argc != 2) crash("usage: %s /path/to/socket\n", argv0);
+
+	while ((ch = getopt(argc, argv, "dhiov")) != -1){
+		switch (ch) {
+		case 'd': ++debug; break;
+		case 'v': ++verbose; break;
+		case 'o': ++noin; break;
+		case 'i': ++noout; break;
+		case 'h':
+		default:
+			usage();
+		}
+	}
+
+	argv += optind;
+	argc -= optind;
+
+	if (argc < 1) usage();
 
         int s = socket(AF_LOCAL, SOCK_STREAM, 0);
         if (s < 0) crash("socket");
         struct sockaddr_un addr;
         addr.sun_family = AF_LOCAL;
-        strncpy(addr.sun_path, argv[1], sizeof(addr.sun_path));
+        strncpy(addr.sun_path, argv[0], sizeof(addr.sun_path));
 
         if (connect(s, (struct sockaddr*)&addr, sizeof(addr)) < 0)
-                crash("connect(%s)", argv[1]);
+                crash("connect(%s)", argv[0]);
 
-        pid_t s_to_out_pid = fork();
-        if (s_to_out_pid == 0) {
-                FILE* s_in = fdopen(s, "r");
-                setlinebuf(stdout);
-                char line[1024];
-                while (!feof(s_in))
-                        if (fgets(line, sizeof(line), s_in))
-                                fputs(line, stdout);
-                exit(0);
-        }
-        if (s_to_out_pid < 0) crash("fork(socket to out)");
+	argc--; argv++;
 
-        pid_t in_to_s_pid  = fork();
-        if (in_to_s_pid == 0) {
-                FILE* s_out = fdopen(s, "w");
-                setlinebuf(s_out);
-                char line[1024];
-                while (!feof(stdin))
-                        if (fgets(line, sizeof(line), stdin))
-                                fputs(line, s_out);
-                exit(0);
-        }
-        if (in_to_s_pid < 0) crash("fork(in to socket)");
+	if (argc) {
+		if (noin) close(0); else dup2(s, 0);
+		if (noout) close(1); else dup2(s, 1); 
+		execvp(argv[0], argv);
+		crash("Failed to exec %s", argv[0]);
+		return 0;
+	}
 
-        if (wait(NULL) < 0) crash("wait");
+	// Else, no command, but cat stdin and stdout linewise
 
-        kill(in_to_s_pid, SIGTERM);
-        kill(s_to_out_pid, SIGTERM);
+	if (noout) {
+		fclose(stdout);
+	} else {
+		s_to_out_pid = fork();
+		if (s_to_out_pid == 0) {
+			FILE* s_in = fdopen(s, "r");
+			setlinebuf(stdout);
+			char line[1024];
+			while (!feof(s_in))
+				if (fgets(line, sizeof(line), s_in))
+					fputs(line, stdout);
+			exit(0);
+		}
+		if (s_to_out_pid < 0) crash("fork(socket to out)");
+	}
+
+	if (noin) {
+		fclose(stdin);
+	} else {
+		in_to_s_pid  = fork();
+		if (in_to_s_pid == 0) {
+			FILE* s_out = fdopen(s, "w");
+			setlinebuf(s_out);
+			char line[1024];
+			while (!feof(stdin))
+				if (fgets(line, sizeof(line), stdin))
+					fputs(line, s_out);
+			exit(0);
+		}
+		if (in_to_s_pid < 0) crash("fork(in to socket)");
+	}
+
+	if ((in_to_s_pid != -1) || (s_to_out_pid != -1)) {
+			if (wait(NULL) < 0) crash("wait");
+	}
+
+	if (in_to_s_pid  != -1) kill(in_to_s_pid, SIGTERM);
+        if (s_to_out_pid != -1) kill(s_to_out_pid, SIGTERM);
 
         return 0;
 }
