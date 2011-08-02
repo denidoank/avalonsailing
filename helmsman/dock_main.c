948c95c2b2f93bb713d55245cec52ffaa1a68393
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-08-02 11:09:03 +0000

    small fixes for build

--- helmsman/dock_main.c
+++ helmsman/dock_main.c
@@ -7,6 +7,7 @@
 //
 #include <errno.h>
 #include <math.h>
+#include <signal.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
--- io/imucat_main.c
+++ io/imucat_main.c
@@ -7,6 +7,7 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
