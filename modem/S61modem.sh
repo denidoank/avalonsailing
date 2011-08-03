50bf9938c5678635adf48e8399ff01bddc7f8e87
mariusv@google.com <mariusv@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-08-03 07:30:17 +0000

    Update input/output flags for modem/status init services.

--- modem/S61modem.sh
+++ modem/S61modem.sh
@@ -13,7 +13,7 @@ case "$1" in
   start)
         echo -n "Starting $DESC: "
 	test -d $QUEUE || mkdir -p $QUEUE
-	$DIR/modemd --no-syslog --task=modemd --timeout=60 --debug --device=$DEVICE --phone=$PHONE --queue=$QUEUE | $DIR/plug -o /var/run/lbus >/dev/null 2>&1 &
+	$DIR/modemd --no-syslog --task=modemd --timeout=60 --debug --device=$DEVICE --phone=$PHONE --queue=$QUEUE | $DIR/plug -i /var/run/lbus >/dev/null 2>&1 &
         echo "OK"
         ;;
   stop)
---
+++ proto/modem.h
@@ -0,0 +1,41 @@
+#ifndef PROTO_MODEM_H
+#define PROTO_MODEM_H
+
+#include <math.h>
+#include <time.h>
+
+struct ModemProto {
+  time_t timestamp_s;
+  int inbox_queue_messages;
+  int outbox_queue_messages;
+  int network_registration;
+  int signal_quality;
+  double coarse_position_lat;
+  double coarse_position_lng;
+  time_t position_timestamp_s;
+};
+
+#define INIT_MODEMPROTO {0, 0, 0, false, 0, NAN, NAN, 0}
+
+// For use in printf and friends.
+#define OFMT_MODEMPROTO(x, n)                                  \
+        "modem: timestamp_ms:%ld inbox_queue_messages:%d "     \
+        "outbox_queue_messages:%d network_registration:%d "    \
+        "signal_quality:%d coarse_position_lat:%.3lf "         \
+        "coarse_position_lng:%.3lf position_timestamp:%ld %n", \
+        (x).timestamp_s, (x).inbox_queue_messages,             \
+        (x).outbox_queue_messages, (x).network_registration,   \
+        (x).signal_quality, (x).coarse_position_lat,           \
+        (x).coarse_position_lng, (x).position_timestamp_s, (n)
+
+#define IFMT_MODEMPROTO(x, n)                                    \
+        "modem: timestamp_ms:%ld inbox_queue_messages:%d "       \
+        "outbox_queue_messages:%d network_registration:%d "      \
+        "signal_quality:%d coarse_position_lat:%lf "             \
+        "coarse_position_lng:%lf position_timestamp:%ld %n",     \
+        &(x)->timestamp_s, &(x)->inbox_queue_messages,           \
+        &(x)->outbox_queue_messages, &(x)->network_registration, \
+        &(x)->signal_quality, &(x)->coarse_position_lat,         \
+        &(x)->coarse_position_lng, &(x)->position_timestamp_s, (n)
+
+#endif // PROTO_MODEM_H
