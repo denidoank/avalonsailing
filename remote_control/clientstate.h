910dcd048c8f036b6fa0f413f79f237b59da258a
jpilet@google.com <jpilet@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-08-05 09:00:34 +0000

    remote_control: displays compass. Fixed a few bugs.
    Added a compass visual display.
    Simplified the drawing update code: there's now a single update event that will
    read values from the state object.
    Fixed confusion between ruddersts and rudderctl.

--- remote_control/clientstate.h
+++ remote_control/clientstate.h
@@ -18,10 +18,6 @@ class ClientState : public QObject
 {
   Q_OBJECT
 
-  Q_PROPERTY(float rudder_l_deg READ get_rudder_l_deg WRITE set_rudder_l_deg NOTIFY rudder_l_deg_changed)
-  Q_PROPERTY(float rudder_r_deg READ get_rudder_r_deg WRITE set_rudder_r_deg NOTIFY rudder_r_deg_changed)
-  Q_PROPERTY(float sail_deg READ get_sail_deg WRITE set_sail_deg NOTIFY sail_deg_changed)
-
 public:
   explicit ClientState(QObject *parent = 0);
 
@@ -29,23 +25,17 @@ public:
 
   const QMap<QString, QMap<QString, QString> >& getData() const { return data_; }
 
-  void updateProperty(const QString& key, const QString& val);
-
-  float get_rudder_l_deg() const { return rudder_l_deg_; }
-  void set_rudder_l_deg(float angle) { rudder_l_deg_ = angle; emit rudder_l_deg_changed(angle); }
-  float get_rudder_r_deg() const { return rudder_r_deg_; }
-  void set_rudder_r_deg(float angle) { rudder_r_deg_ = angle; emit rudder_r_deg_changed(angle); }
-  float get_sail_deg() const { return sail_deg_; }
-  void set_sail_deg(float angle) { sail_deg_ = angle; emit sail_deg_changed(angle); }
+  QString getVar(const QString& daemon, const QString& var) const {
+        return data_.value(daemon).value(var);
+  }
+  double getDouble(const QString& daemon, const QString& var) const {
+          return getVar(daemon, var).toDouble();
+  }
 
 signals:
 
   void dataUpdate();
   void consoleOutput(QString text);
-
-  void rudder_l_deg_changed(float angle);
-  void rudder_r_deg_changed(float angle);
-  void sail_deg_changed(float angle);
 
 public slots:
   void setCommand(const QString& command);
@@ -65,7 +55,6 @@ private:
   QMap<QString, QMap<QString, QString> > data_;
 
   QTimer restart_timer_;
-  float rudder_r_deg_, rudder_l_deg_, sail_deg_;
 };
 
 #endif // CLIENTSTATE_H
