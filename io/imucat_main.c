69f9e260712222e1cf5dd36b90fed56e9d0cd2c8
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-08-08 12:58:25 +0000

    io/imucfg: tool to configure IMU in the field.

--- io/imucat_main.c
+++ io/imucat_main.c
@@ -6,7 +6,6 @@
 // 
 
 #include <errno.h>
-#include <fcntl.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdint.h>
@@ -19,6 +18,7 @@
 #include <unistd.h>
 
 #include "../proto/imu.h"
+#include "mtcp.h"
 
 // -----------------------------------------------------------------------------
 //   Together with getopt in main, this is our minimalistic UI
@@ -63,57 +63,6 @@ usage(void)
 		, argv0);
 	exit(2);
 }
-
- 
-enum {
-	IMU_REQCONFIGURATION	= 0x0C, IMU_CONFIGURATION,
-	IMU_GOTOMEASUREMENT	= 0x10,	IMU_GOTOMEASUREMENTACK,
-	IMU_GOTOCONFIG		= 0x30,	IMU_GOTOCONFIGACK,
-	IMU_MTDATA		= 0x32,
-	IMU_WAKEUP		= 0x3E,	IMU_WAKEUPACK,
-	IMU_ERROR		= 0x42,
-};
-
-// we use: temp, calibr.data orientation data, position data velocity data, status data, timestamp=utc time
-// orientation mode = euler, datafmt = standard, send acc, send gyr, !send.mag
-// mt msg len = 82  (0xFA 0xFF 0x32 0x4D ...
-enum {
-	// Output mode, message id 0xD0, 16 bits
-	IMU_OM_TMP = 1<<0,
-	IMU_OM_CAL = 1<<1,
-	IMU_OM_ORI = 1<<2,
-	IMU_OM_AUX = 1<<3,
-	IMU_OM_POS = 1<<4,
-	IMU_OM_VEL = 1<<5,
-	IMU_OM_STS = 1<<11,
-
-	// can't combine with the others, don't use
-	IMU_OM_GPS = 1<<12,
-	IMU_OM_RAW = 1<<14,
-
-	// Output settings, message id 0xD2
-	// timestamp: 0 is none
-	IMU_OS_TS_SC = 1<<0,   // timestamp: sample counter
-	IMU_OS_TS_UTC = 1<<1,  // timestamp: utc time
-
-	// orientation output mode, 0 is quaternions.  don't mix the following
-	IMU_OS_OR_EULER = 1<<2,
-	IMU_OS_OR_MATRIX = 1<<3,
-	IMU_OS_OR_MASK = IMU_OS_OR_EULER|IMU_OS_OR_MATRIX,
-
-	// calibr mode disable bits
-	IMU_OS_CM_DISACC = 1<<4,
-	IMU_OS_CM_DISGYR = 1<<5,
-	IMU_OS_CM_DISMAG = 1<<6,
-
-	// bit 7 reserved, 8,9 float format: use 0: default.
-	IMU_OS_FF_MASK = (1<<8|1<<9),
-	// bit 10, 11: aux mode
-	IMU_OS_AU_DIS1 = (1<<10),
-	IMU_OS_AU_DIS2 = (1<<11),
-
-	//  rest: we don't use either
-};
 
 static float
 decode_float(uint8_t** dd)
@@ -256,10 +205,6 @@ imu_decode_variables(uint8_t* b, int len, uint16_t mode, uint32_t settings, stru
 }
 
 // -----------------------------------------------------------------------------
-enum {
-	IMU_OUTPUT_MODE = IMU_OM_TMP|IMU_OM_CAL|IMU_OM_ORI|IMU_OM_POS|IMU_OM_VEL|IMU_OM_STS,
-	IMU_OUTPUT_SETTINGS = IMU_OS_TS_UTC|IMU_OS_OR_EULER,
-};
 
 int main(int argc, char* argv[]) {
 
@@ -279,13 +224,11 @@ int main(int argc, char* argv[]) {
 		case 'v': ++verbose; break;
 		case 'm':
 		  mode = strtol(optarg, NULL, 0); 
-		  if (errno == ERANGE)
-		    crash("can't parse %s as a number\n", optarg);
+		  if (errno == ERANGE) crash("can't parse %s as a number\n", optarg);
 		  break;
 		case 's':
 		  settings = strtoul(optarg, NULL, 0);
-		  if (errno == ERANGE)
-		    crash("can't parse %s as a number\n", optarg);
+		  if (errno == ERANGE) crash("can't parse %s as a number\n", optarg);
 		  break;
 		case 'h': 
 		default:
