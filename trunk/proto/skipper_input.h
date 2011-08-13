19b2c94ff240651eb5f2d532724cb0cdd0244fb7
grundmann@google.com <grundmann@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-08-13 10:34:30 +0000

    component: Proto for skipper input
    R=lvd,rekwall
    I=4893041

---
+++ trunk/proto/skipper_input.h
@@ -0,0 +1,38 @@
+// Copyright 2011 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+// Grundmann, August 2011
+// Skipper input (position and true wind).
+
+#ifndef PROTO_SKIPPER_INPUT_H
+#define PROTO_SKIPPER_INPUT_H
+
+#include <math.h>
+#include <time.h>
+
+struct RemoteProto {
+  time_t timestamp_s;
+  // GPS data in degrees
+  // Both values are NAN initially.
+  double longitude_deg;
+  double latitude_deg;
+
+  // true wind, global frame
+  // Both values are NAN initially.
+  double angle_true_deg;
+  double mag_true_kn;
+};
+
+#define INIT_SKIPPER_INPUTPROTO {0, NAN, NAN, NAN, NAN}
+
+// For use in printf and friends.
+#define OFMT_SKIPPER_INPUTPROTO(x, n)                             \
+  "remote: timestamp_s:%ld longitude_deg:%lf latitude_deg:%lf angle_true_deg:%lf mag_true_kn:%6.4lf%n",  \
+    (x).timestamp_s, (x).longitude_deg, (x). latitude_deg, (x). angle_true_deg, (x). mag_true_kn, (n)
+
+// For use in scanf.
+#define IFMT_SKIPPER_INPUTPROTO(x, n)                             \
+  "remote: timestamp_s:%ld longitude_deg:%lf latitude_deg:%lf angle_true_deg:%lf mag_true_kn:%lf%n",	   \
+   &(x).timestamp_s, &(x).longitude_deg, &(x).latitude_deg, &(x).angle_true_deg, &(x).mag_true_kn, (n)
+
+#endif // PROTO_SKIPPER_INPUT_H
