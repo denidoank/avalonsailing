f194fcde060a1a36d43a94ca1ab04bc4b540f462
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-08-23 21:43:38 +0000

    io/linebus: fctor out linebuffer for use in other programs

---
+++ trunk/io/linebuffer.h
@@ -0,0 +1,23 @@
+// Copyright 2011 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+
+// line buffer that plays well with select
+struct LineBuffer {
+	char line[1024];
+	int head;
+	int discard;
+	int tail;
+};
+
+// Reads from fd into the linebuffer, returns 0 if there is a \n terminated
+// line, EAGAIN if there is an incomplete line, or errno from read(2)
+// otherwise.  if the buffer overflows before EOL sets the discard flag which will
+// cause the current line to be discarded until an EOL is found.
+// the line will be 0-terminated.
+int lb_read(int fd, struct LineBuffer* lb);
+
+// Writing, call lb_putline and then lb_write until it returns 0
+void lb_putline(struct LineBuffer* lb, const char* line);
+int lb_pending(struct LineBuffer* lb);
+int lb_write(int fd, struct LineBuffer* lb);
