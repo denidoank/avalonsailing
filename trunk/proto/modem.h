de2eb61ebbec273d676c47b49c539df4e65fe9e7
mariusv@google.com <mariusv@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2011-12-09 15:15:39 +0000

    Digital compass: update proto format Modem: update proto format
    R=grundmann
    I=5467050

--- trunk/proto/modem.h
+++ trunk/proto/modem.h
@@ -22,7 +22,7 @@ struct ModemProto {
         "modem: timestamp_ms:%ld inbox_queue_messages:%d "     \
         "outbox_queue_messages:%d network_registration:%d "    \
         "signal_quality:%d coarse_position_lat:%.3lf "         \
-        "coarse_position_lng:%.3lf position_timestamp:%ld %n", \
+        "coarse_position_lng:%.3lf position_timestamp:%ld\n",  \
         (x).timestamp_s, (x).inbox_queue_messages,             \
         (x).outbox_queue_messages, (x).network_registration,   \
         (x).signal_quality, (x).coarse_position_lat,           \
@@ -32,7 +32,7 @@ struct ModemProto {
         "modem: timestamp_ms:%ld inbox_queue_messages:%d "       \
         "outbox_queue_messages:%d network_registration:%d "      \
         "signal_quality:%d coarse_position_lat:%lf "             \
-        "coarse_position_lng:%lf position_timestamp:%ld %n",     \
+        "coarse_position_lng:%lf position_timestamp:%ld\n%n",    \
         &(x)->timestamp_s, &(x)->inbox_queue_messages,           \
         &(x)->outbox_queue_messages, &(x)->network_registration, \
         &(x)->signal_quality, &(x)->coarse_position_lat,         \
