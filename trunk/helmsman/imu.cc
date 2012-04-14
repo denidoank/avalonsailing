89e102dabdd29a3e6134fae322b69b1b23c19b9d
grundmann@google.com <grundmann@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-04-14 15:17:25 +0000

    Magnet field vector in Controller IMU.

--- trunk/helmsman/imu.cc
+++ trunk/helmsman/imu.cc
@@ -30,16 +30,20 @@ void Imu::Reset() {
   gyro.omega_y_rad_s = 0;
   gyro.omega_z_rad_s = 0;
   temperature_c = 20;
+  mag.raw_x = 0;
+  mag.raw_y = 0;
+  mag.raw_z = 0;
 }
 
 std::string Imu::ToString() const {
-  char line[17*25];
+  char line[20*25];
   int s = snprintf(line, sizeof line, "speed_m_s: %f lat_deg:%f lon_deg:%f alt_m:%f "
       "phi_x:%.4f phi_y:%.4f phi_z:%.4f "
       "v_x:%.4f v_y:%.4f v_z:%.4f "
       "acc_x:%.4f acc_y:%.4f acc_z:%.4f "
       "om_x:%.4f om_y:%.4f om_z:%.4f "
       "theta: %.3f"
+      "mag_x:%.4f mag_y:%.4f mag_z:%.4f "
       "\n",
       speed_m_s,
       position.longitude_deg, position.latitude_deg, position.altitude_m,
@@ -47,34 +51,38 @@ std::string Imu::ToString() const {
       velocity.x_m_s, velocity.y_m_s, velocity.z_m_s,
       acceleration.x_m_s2, acceleration.y_m_s2, acceleration.z_m_s2,
       gyro.omega_x_rad_s, gyro.omega_y_rad_s, gyro.omega_z_rad_s,
-      temperature_c);
+      temperature_c,
+      mag.raw_x, mag.raw_y, mag.raw_z);
   return std::string(line, s);
 }
 
 void Imu::ToProto(IMUProto* imu_proto) const {
-imu_proto->temp_c = temperature_c;
+  imu_proto->temp_c = temperature_c;
 
-imu_proto->acc_x_m_s2 = acceleration.x_m_s2;
-imu_proto->acc_y_m_s2 = acceleration.y_m_s2;
-imu_proto->acc_z_m_s2 = acceleration.z_m_s2;
+  imu_proto->acc_x_m_s2 = acceleration.x_m_s2;
+  imu_proto->acc_y_m_s2 = acceleration.y_m_s2;
+  imu_proto->acc_z_m_s2 = acceleration.z_m_s2;
 
-imu_proto->gyr_x_rad_s = gyro.omega_x_rad_s ;
-imu_proto->gyr_y_rad_s = gyro.omega_y_rad_s ;
-imu_proto->gyr_z_rad_s = gyro.omega_z_rad_s ;
+  imu_proto->gyr_x_rad_s = gyro.omega_x_rad_s ;
+  imu_proto->gyr_y_rad_s = gyro.omega_y_rad_s ;
+  imu_proto->gyr_z_rad_s = gyro.omega_z_rad_s ;
 
-imu_proto->roll_deg  = Rad2Deg(attitude.phi_x_rad);
-imu_proto->pitch_deg = Rad2Deg(attitude.phi_y_rad);
-imu_proto->yaw_deg   = NormalizeDeg(Rad2Deg(attitude.phi_z_rad));
+  imu_proto->roll_deg  = Rad2Deg(attitude.phi_x_rad);
+  imu_proto->pitch_deg = Rad2Deg(attitude.phi_y_rad);
+  imu_proto->yaw_deg   = NormalizeDeg(Rad2Deg(attitude.phi_z_rad));
 
-imu_proto->lat_deg = position.latitude_deg;
-imu_proto->lng_deg = position.longitude_deg;
-imu_proto->alt_m   = position.altitude_m;
+  imu_proto->lat_deg = position.latitude_deg;
+  imu_proto->lng_deg = position.longitude_deg;
+  imu_proto->alt_m   = position.altitude_m;
 
- // Speed == x component of motion vector for our purposes.
-imu_proto->vel_x_m_s = speed_m_s;
-imu_proto->vel_x_m_s = velocity.x_m_s;
-imu_proto->vel_y_m_s = velocity.y_m_s;
-imu_proto->vel_z_m_s = velocity.z_m_s;
+   // Speed components in NED coordinate frame
+  imu_proto->vel_x_m_s = velocity.x_m_s;
+  imu_proto->vel_y_m_s = velocity.y_m_s;
+  imu_proto->vel_z_m_s = velocity.z_m_s;
+
+  imu_proto->mag_x_au = mag.raw_x;
+  imu_proto->mag_y_au = mag.raw_y;
+  imu_proto->mag_z_au = mag.raw_z;
 }
 
 void Imu::FromProto(const IMUProto& imu_proto) {
@@ -101,4 +109,8 @@ void Imu::FromProto(const IMUProto& imu_proto) {
   velocity.x_m_s = imu_proto.vel_x_m_s;
   velocity.y_m_s = imu_proto.vel_y_m_s;
   velocity.z_m_s = imu_proto.vel_z_m_s;
+
+  mag.raw_x = imu_proto->mag_x_au;
+  mag.raw_y = imu_proto->mag_y_au;
+  mag.raw_z = imu_proto->mag_z_au;
 }
