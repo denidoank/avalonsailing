b87b1e9d16381b5ea9ee37e6f66d0a2b67c0330a
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-04-23 16:19:00 +0000

    start_avalon.sh fixes.

--- start_avalon.sh
+++ start_avalon.sh
@@ -19,26 +19,34 @@ PORT_MODEM=/dev/ttyUSB6
 PORT_WIND=/dev/ttyUSB7
 PORT_IMU=/dev/ttyUSB8	#  /dev/xsensIMU
 
+EBUS=/tmp/ebus  # /var/run/ebus
+LBUS=/tmp/lbus  # /var/run/lbus
+
+export PATH=/tmp/bin:$PATH  # TODO
 #cd /var/run
 cd /tmp
 
+err=""
+for p in linebusd plug epos probe ruddersts rudderctlfwd eposmon eposcom rudderctl sailctl\
+	imucfg imucat aiscat aisbuf compasscat windcat fcmon; do
+    which $p >/dev/null 2>&1 || err="$err $p"
+done
+
+if [ "$err" != "" ]; then
+    echo "Missing binaries: $err"
+    echo "quitting."
+    exit 1
+fi
+
 logger -s -p local2.Notice "Avalon subsystems starting up"
 
-linebusd ./lbus
-linebusd ./ebus
+linebusd $LBUS
 
 while /bin/true
 do
+    logger -s -p local2.Notice "(Re)Starting epos subsystems"
 
-    (
-	# eposprobe: periodically issue status register probe commands (needed by ruddersts and -mon)
-	# ruddersts: decode status registers to ruddersts: messages
-	# rudderctlfwd: forward rudderctl: messages to ebus, prefixed with '#'
-	# eposmon: summarize and report epos communication errors to syslog
-	eposprobe | plug $EBUS | ruddersts | plug $LBUS | rudderctlfwd | plug $EBUS | eposmon
-	logger -s -p local2.crit "Epos status subsystem exited."
-	kill `cat $EBUS.pid`
-    )&
+    linebusd $EBUS	# blocks until socket exists
 
     for p in $PORT_RUDDER_L  $PORT_RUDDER_R $PORT_SAIL 
     do
@@ -49,41 +57,48 @@ do
 	)&
     done
 
-    (while /bin/true; do
+    (
 	plug $EBUS -- rudderctl -l		# homing and positioning of left rudder
 	logger  -s -p local2.crit "Rudderctl -l exited."
-	sleep 5
-    done) &
+	kill `cat $EBUS.pid`
+    )&
 
-    (while /bin/true; do
+    (
 	plug $EBUS -- rudderctl -r		# homing and positioning of right rudder
 	logger  -s -p local2.crit "Rudderctl -r exited."
-	sleep 5
-    done) &
+	kill `cat $EBUS.pid`
+    )&
 
-    (while /bin/true; do
+    (
 	plug $EBUS --  sailctl  &		# positioning for sail
 	logger  -s -p local2.crit "Sailctl exited."
-	sleep 5
-    done) &
+	kill `cat $EBUS.pid`
+    )&
 
+    # eposprobe: periodically issue status register probe commands (needed by ruddersts and -mon)
+    # ruddersts: decode status registers to ruddersts: messages
+    # rudderctlfwd: forward rudderctl: messages to ebus, prefixed with '#'
+    # eposmon: summarize and report epos communication errors to syslog
+    eposprobe | plug $EBUS | ruddersts | plug $LBUS | rudderctlfwd | plug $EBUS | eposmon
+    logger -s -p local2.crit "Epos status subsystem exited."
+    kill `cat $EBUS.pid`
+
+    sleep 5
 done
-
 
 exit  # for now
 
-imucfg && imucat /dev/xsensIMU | plug -i /var/run/lbus &
+(imucfg $PORT_IMU && imucat $PORT_IMU | ./plug -i $LBUS ; logger  -s -p local2.crit "imucat exited.")&
+(compasscat $PORT_COMPASS | plug -i $LBUS;  logger  -s -p local2.crit "compasscat exited.")&
+(windcat $PORT_WIND | plug -i $LBUS;  logger -s -p local2.crit "windcat exited.")&
+(fcmon $PORT_FUELCELL | plug -i $LBUS ;  logger -s -p local2.crit "fcmon exited.")&
 
-windcat  | plug -i /var/run/lbus &
-aiscat  /dev/ttyUSB7    | aisbuf /var/run/ais.txt &
-fcmon   /dev/ttyUSB5    | plug -i /var/run/lbus &
+# (aiscat $PORT_AIS | aisbuf /var/run/ais.txt; logger -s -p local2.crit "aiscat exited.")&
 
-plug /var/run/lbus helmsman 2>/dev/null
+plug $LBUS -- helmsman 2>&1 | logger -p local2.debug
 
-modemd --device=/dev/ttyUSB8 --queue=/var/run/modem | plug -i /var/run/lbus 2>/dev/null &
-
-plug -o /var/run/lbus | statusd  --queue=/var/run/modem \
---initial_timeout=180 --status_interval=86400 --remote_cmd_interval=5 >/dev/null 2>&1 &
+# modemd --device=$PORT_MODEM --queue=/var/run/modem | plug $LBUS | statusd  --queue=/var/run/modem \
+#	--initial_timeout=180 --status_interval=86400 --remote_cmd_interval=5 
 
 echo "Und immer eine Handbreit Wasser unter dem Kiel!"
 echo "May she always have a good passage, wherever she goes!"
