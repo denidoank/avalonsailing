65a29479a51e9517e0dd47ac74911d3a33f6c4a8
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-05-07 23:07:34 +0000

    io: added start/top timer stats facility + first use in linebusd

---
+++ io/timer.c
@@ -0,0 +1,128 @@
+// Copyright 2012 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+//
+#include "timer.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+int64_t now_us() {
+        struct timeval tv;
+        if (gettimeofday(&tv, NULL) < 0) {
+                fprintf(stderr, "no working clock");
+                exit(1);
+        }
+
+        int64_t us1 = tv.tv_sec;  us1 *= 1000000;
+        int64_t us2 = tv.tv_usec;
+        return us1 + us2;
+}
+
+int64_t timer_tick(struct Timer* t,  int64_t now, int start) {
+	start = start ? 0 : 1;
+	if (start == (t->count & 1) && t->ignoredups) return 0;
+	int64_t last_start = now;
+	if(t->count) last_start = t->ticks[ ((t->count-1) % (2*TIMER_EVENTS)) & ~1 ];
+	t->ticks[((t->count++ % (2*TIMER_EVENTS)) & ~1) | start] = now;
+	return now - last_start;
+}
+
+int64_t timer_tick_now(struct Timer* t, int start) { return timer_tick(t, now_us(), start); }
+int timer_running(struct Timer* t) { return t->count & 1; }
+void timer_reset(struct Timer* t) { t->count = 0; }
+
+int timer_stats(struct Timer*t, struct TimerStats *s) {
+	if(timer_running(t)) return 1;
+
+	memset(s, 0, sizeof *s);
+	s->count = t->count / 2;
+	if (s->count < 2) return 1;
+
+	int nn = (s->count < TIMER_EVENTS) ? s->count : TIMER_EVENTS; // number of events in queue
+	int last = (s->count - 1) % TIMER_EVENTS;  // last written event
+	int first = (s->count < TIMER_EVENTS) ? 0 : (s->count % TIMER_EVENTS) ; // first event in queue
+
+	int i, j;
+	double sx, sxx, sy, syy;
+
+	// average period is  last stop - first start / (nn + .5)
+	sx = t->ticks[2*last] - t->ticks[2*first];
+	s->pavg = sx / (nn + .5);
+
+	// min/average/max run time 
+	sx = 0;
+	s->rmin = HUGE_VAL; s->rmax = -HUGE_VAL;
+	for(i = 0; i < nn; ++i) {
+		double x = t->ticks[2*i + 1] - t->ticks[2*i];
+		if (s->rmin > x) s->rmin = x;
+		if (s->rmax < x) s->rmax = x;
+		sx += x;
+	}
+	sx /= nn;
+	s->ravg = sx;
+
+	sxx = 0;
+	for(i = 0; i < nn; ++i) {
+		double x = t->ticks[2*i + 1] - t->ticks[2*i];
+		x -= sx;
+		sxx += x*x;
+	}
+	s->rdev = sqrt(sxx/nn);
+
+	i = 0;
+	j = 1;
+	sxx = 0;
+	sx = s->pavg;
+	while(i < nn) {
+		if (i != last) {
+			double x = t->ticks[2*j] - t->ticks[2*i];      // period
+			if (s->pmin > x) s->pmin = x;
+			if (s->pmax < x) s->pmax = x;
+
+			x -= sx;
+			sxx += x*x;
+		}
+		i = (i + 1) % TIMER_EVENTS;
+		j = (j + 1) % TIMER_EVENTS;
+	}
+	s->pdev = sqrt(sxx/(nn-1));
+
+	i = 0;
+	j = 1;
+	sx = 0;
+	while(i < nn) {
+		if (i != last) {
+			double x = t->ticks[2*j] - t->ticks[2*i];      // period
+			double y = t->ticks[2*i + 1] - t->ticks[2*i];  // active
+			double yx = (x == 0.0) ? 1 : y/x;
+			sx += yx;
+		}
+		i = (i + 1) % TIMER_EVENTS;
+		j = (j + 1) % TIMER_EVENTS;
+	}
+	sx /= (nn-1);
+	s->davg = sx;
+
+	i = 0;
+	j = 1;
+	sxx = 0;
+	while(i < nn) {
+		if (i != last) {
+			double x = t->ticks[2*j] - t->ticks[2*i];      // period
+			double y = t->ticks[2*i + 1] - t->ticks[2*i];  // active
+			double yx = (x == 0.0) ? 1 : y/x;
+			yx -= sx;
+			sxx += yx * yx;
+		}
+		i = (i + 1) % TIMER_EVENTS;
+		j = (j + 1) % TIMER_EVENTS;
+	}
+	s->ddev = sqrt(sxx /(nn-1)); 
+	
+	return 0;
+}
+
---
+++ io/timer.h
@@ -0,0 +1,61 @@
+// Copyright 2012 The Avalon Project Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+//
+// Statistics keeping start-stop timer.  Keeps running average
+// period, duty cycle and max/min period and up over the last 64
+// (TIMER_EVENTS) cycles.
+//
+//
+#ifndef _IO_TIMER_H
+#define _IO_TIMER_H
+
+#include <stdint.h>
+
+
+enum { TIMER_EVENTS = 64 };  // number of events to keep in timer ringbuffer
+
+struct Timer {
+	int64_t ticks[2*TIMER_EVENTS];  // starts(even) and stops(odd) ringbuffer
+	int64_t count;		        // even: timer stopped, odd timer running.
+	int ignoredups;		        // if set, repeated starts and stops are ignored.
+};
+
+int64_t now_us();  // current time in microseconds.  calls gettimeofday(2).
+
+// Register that the timer started (start != 0) or stopped (start == 0)at now_us.
+// repeated calls with the same value of start are ignored (t->ignoredup is true)
+// or cause the current event to be updated (default).
+// Returns the number of microseconds since last start.
+int64_t timer_tick(struct Timer* t,  int64_t now, int start);
+
+// equivalent to timer_tick(t, now_us(), start)
+int64_t timer_tick_now(struct Timer* t, int start);
+
+// returns true if the timmer is currently started.
+int timer_running(struct Timer* t);
+
+// clear all events from the timer (by resetting count)
+void timer_reset(struct Timer* t);
+
+struct TimerStats {
+	uint64_t count;
+	double pmin, pavg, pdev, pmax;
+	double rmin, ravg, rdev, rmax;
+	double davg, ddev;
+};
+
+// Compute the timer stats over the ringbuffer.
+// Count is the total number of events timed. the stats are only over the last TIMER_EVENTS.
+// On succes, returns zero. If the timer is currently running or has
+// fewer than 2 starts, will do nothing and return 1.
+int timer_stats(struct Timer*t, struct TimerStats *s);
+
+#define OFMT_TIMER_STATS(s) \
+	"count:%lld period(ms): %.3lf / %.3lf (±%.3lf) / %.3lf run(ms): %.3lf / %.3lf (±%.3lf) / %.3lf dutycycle(%%): %.1lf (±%.1lf)", \
+	(s).count, \
+		(s).pmin/1000, (s).pavg/1000, (s).pdev/1000, (s).pmax/1000, \
+		(s).rmin/1000, (s).ravg/1000, (s).rdev/1000, (s).rmax/1000, \
+		(s).davg*100, (s).ddev*100
+
+#endif // _IO_TIMER_H
