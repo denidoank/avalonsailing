fc1ee626b119bb763d54d41a88c39f9540476d17
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-05-07 23:08:53 +0000

    fix stats cmd in linebusd + added TODO

--- io/linebusd_main.c
+++ io/linebusd_main.c
@@ -288,10 +288,11 @@ handle_cmd(struct Client* client, char* line) {
 	if (strcmp("stats", line) == 0) {
 		char buf[1024];
 		struct Client* cl;
+		// TODO: per client read/write timers
 		for(cl = clients; cl; cl=cl->next) {
 			snprintf(buf, sizeof buf, "%d %s dropped: %d\n", cl->fd, client_name(cl), cl->dropped); 
 			cl->dropped = 0;
-			client_puts(cl, buf);
+			client_puts(client, buf);
 		}
 		return;
 	}
