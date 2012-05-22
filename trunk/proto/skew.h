f8e24f0516821a5cd192745482107df038f5e7ee
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-05-22 09:05:53 +0000

    added skew proto

---
+++ trunk/proto/skew.h
@@ -0,0 +1,19 @@
+#ifndef PROTO_SKEW_H
+#define PROTO_SKEW_H
+
+#include <math.h>
+#include <stdint.h>
+
+struct SkewProto {
+	int64_t timestamp_ms;
+	double angle_deg;
+};
+
+#define INIT_SKEWPROTO {0, NAN}
+
+// For use in printf and friends.
+#define OFMT_SKEWPROTO(x)    "skew: timestamp_ms:%lld angle_deg:%.3lf\n", (x).timestamp_ms, (x).angle_deg
+
+#define IFMT_SKEWPROTO(x, n) "skew: timestamp_ms:%lld angle_deg:%lf\n%n", &(x)->timestamp_ms, &(x)->angle_deg, (n)
+
+#endif // PROTO_WIND_H
