f304204c7f0f1a3073fc34698e334cad8b80ee11
grundmann@google.com <grundmann@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-06-13 09:30:26 +0000

    Remove storm_flag. Add constants for expected sscanf return values.

--- trunk/proto/rudder.h
+++ trunk/proto/rudder.h
@@ -9,27 +9,27 @@ struct RudderProto {
 	double rudder_l_deg;
 	double rudder_r_deg;
 	double sail_deg;
-	int storm_flag;
 };
 
-#define INIT_RUDDERPROTO { 0, NAN, NAN, NAN, 0 }
+#define INIT_RUDDERPROTO { 0, NAN, NAN, NAN }
 
 #define OFMT_RUDDERPROTO_STS(x) \
 	"ruddersts: timestamp_ms:%lld rudder_l_deg:%.1lf rudder_r_deg:%.1lf sail_deg:%.1lf\n" \
 	, (x).timestamp_ms, (x).rudder_l_deg, (x).rudder_r_deg, (x).sail_deg
 
 #define OFMT_RUDDERPROTO_CTL(x) \
-	"rudderctl: timestamp_ms:%lld rudder_l_deg:%.3lf rudder_r_deg:%.3lf sail_deg:%.1lf storm_flag:%d\n" \
-	, (x).timestamp_ms, (x).rudder_l_deg, (x).rudder_r_deg, (x).sail_deg, (x).storm_flag
+	"rudderctl: timestamp_ms:%lld rudder_l_deg:%.3lf rudder_r_deg:%.3lf sail_deg:%.1lf\n" \
+	, (x).timestamp_ms, (x).rudder_l_deg, (x).rudder_r_deg, (x).sail_deg
 
 #define IFMT_RUDDERPROTO_STS(x, n) \
 	"ruddersts: timestamp_ms:%lld rudder_l_deg:%lf rudder_r_deg:%lf sail_deg:%lf\n%n" \
 	, &(x)->timestamp_ms, &(x)->rudder_l_deg, &(x)->rudder_r_deg, &(x)->sail_deg, (n)
+#define IFMT_RUDDERPROTO_STS_ITEMS 4
 
 #define IFMT_RUDDERPROTO_CTL(x, n) \
-	"rudderctl: timestamp_ms:%lld rudder_l_deg:%lf rudder_r_deg:%lf sail_deg:%lf storm_flag:%d\n%n" \
-	, &(x)->timestamp_ms, &(x)->rudder_l_deg, &(x)->rudder_r_deg, &(x)->sail_deg, &(x)->storm_flag, (n)
-
+	"rudderctl: timestamp_ms:%lld rudder_l_deg:%lf rudder_r_deg:%lf sail_deg:%lf\n%n" \
+	, &(x)->timestamp_ms, &(x)->rudder_l_deg, &(x)->rudder_r_deg, &(x)->sail_deg, (n)
+#define IFMT_RUDDERPROTO_CTL_ITEMS 4
 
 // Variants that only touch one field
 #define OFMT_STATUS_LEFT(x) \
