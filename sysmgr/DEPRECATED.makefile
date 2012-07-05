07d467afaf0f7c8fca5bbfe3decbbd23155abe37
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-07-05 14:26:34 +0000

    moved fcmon to io and deleted currently unused systools and sysmgr. we can recover them from the repository when we need them again.

--- sysmgr/DEPRECATED.makefile
+++
@@ -1,6 +0,0 @@
-DEPS+=lib/filter
-DEPS+=lib/config
-DEPS+=io/ipc
-DEPS+=lib/util
-DEPS+=lib/fm
-include ../mk/Makefile.inc
--- sysmgr/sysmon_test.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-./fm_client_test --task-name fmtest  testdev < fmtest.trace &
-./sysmon_test --logtostderr --debug fmtest_sysmgr.conf fmtest_sysmon.conf
