2875eb8462cd350d01332486371f4975cd4390dd
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-07-19 04:20:21 +0000

    added GPRMC sentence decoding and gps proto

---
+++ branches/onboard/proto/gps.h
@@ -0,0 +1,26 @@
+#ifndef PROTO_GPS_H
+#define PROTO_GPS_H
+
+#include <math.h>
+#include <stdint.h>
+
+struct GPSProto {
+	int64_t timestamp_ms;
+	double lat_deg;
+	double lng_deg;
+	double speed_m_s;
+	double cog_deg;
+};
+
+#define INIT_GPSPROTO {0,NAN,NAN,NAN,NAN}
+
+// For use in printf and friends.
+#define OFMT_GPSPROTO(x)						\
+	"gps: timestamp_ms:%lld lat_deg:%.7lf lng_deg:%.7lf speed_m_s:%.3lf cog_deg:%.2lf\n", \
+		(x).timestamp_ms,(x).lat_deg, (x).lng_deg, (x).speed_m_s, (x).cog_deg
+
+#define IFMT_GPSPROTO(x, n)				 \
+	"gps: timestamp_ms:%lld lat_deg:%lf lng_deg:%lf speed_m_s:%lf cog_deg:%lf%n", \
+		&(x)->timestamp_ms, &(x)->lat_deg, &(x)->lng_deg, &(x)->speed_m_s, &(x)->cog_deg, (n)
+
+#endif  // PROTO_GPS_H
