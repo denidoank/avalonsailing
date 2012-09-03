ebe25d2e402e2dadaba6531f075c1c4b05958ae9
lvd@google.com <lvd@google.com@5a7ef71c-b2ad-d1f0-6a96-1ca193062fb2>
2012-09-03 20:18:47 +0000

    attempt nr 2 to understand why helmsman and skipper don't exit

--- trunk/helmsman/helmsman_main.cc
+++ trunk/helmsman/helmsman_main.cc
@@ -79,12 +79,6 @@ void usage(void) {
   exit(2);
 }
 
-static void set_fd(fd_set* s, int* maxfd, FILE* f) {
-        int fd = fileno(f);
-        FD_SET(fd, s);
-        if (*maxfd < fd) *maxfd = fd;
-}
-
 static const int64_t kPeriodMicros = kSamplingPeriod * 1E6;
 
 void AdvanceCallTime(int64_t* next_call_micros) {
@@ -210,17 +204,16 @@ int main(int argc, char* argv[]) {
   for (;;) {
 
     fd_set rfds;
-    int max_fd = -1;
     FD_ZERO(&rfds);
-    set_fd(&rfds, &max_fd, stdin);
+
     sigset_t empty_mask;
     sigemptyset(&empty_mask);
-    int r = pselect(max_fd + 1, &rfds,  NULL, NULL, &timeout, &empty_mask);
+    int r = pselect(fileno(stdin) + 1, &rfds,  NULL, NULL, &timeout, &empty_mask);
     if (r == -1 && errno != EINTR) crash("pselect");
 
     if (debug>2) syslog(LOG_DEBUG, "Woke up %d\n", r);
 
-    if (FD_ISSET(fileno(stdin), &rfds)) {
+    if (r == 1) {
       r = lb_readfd(&lbuf, fileno(stdin));
       if (r == EOF) break;
       if (r == EAGAIN) continue;
